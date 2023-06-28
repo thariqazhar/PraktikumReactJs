ef5d41d8d3eecd1438611f3038429ac64e73694d
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-06-29 05:47:06 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src= "true ayonima1.png" className="App-logo" alt="logo" />
+        <div>
+          <h1>
+            True Entrepreneur
+          </h1>
+          <p>
+              Become a landlord now.
+            </p>
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Do not forget to double the single mother with 3 children rent so that she grind harder.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.zillow.com/rental-manager/resources/how-to-become-landlord/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn to be a landlord
         </a>
       </header>
     </div>
